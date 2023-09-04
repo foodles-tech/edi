d0bc37c339b5d738302bb9c6d08b3006ec7b3dca
Pierre Verkest <pierreverkest84@gmail.com>
2023-09-04 16:36:58 +0200

    FIX unittest based on the lastest webservice refactor

--- edi_webservice_oca/tests/test_send_saving_response.py
+++ edi_webservice_oca/tests/test_send_saving_response.py
@@ -1,6 +1,7 @@
 # Copyright 2023 Foodles (http://www.foodles.co).
 # @author Pierre Verkest <pierreverkest84@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from contextlib import contextmanager
 from unittest import mock
 
 import responses
@@ -27,13 +28,24 @@ class TestSend(TestEDIWebserviceBase):
         """
         cls.record.edi_exchange_state = "output_pending"
 
+        @contextmanager
+        def _consumer_record_no_new_env(self, record, new_cursor=True):
+            yield record
+
+        cls._consumer_record_no_new_env = _consumer_record_no_new_env
+
     @responses.activate
     def test_component_send_save_results_on_success(self):
         self.record.type_id.set_settings(self.settings)
         url = "https://foo.test/push/here"
         response = "{'result': 'ok'}"
         responses.add(responses.POST, url, body=response)
-        self.backend._check_output_exchange_sync()
+        with mock.patch(
+            "odoo.addons.webservice.models.webservice_backend."
+            "WebserviceBackend._consumer_record_env",
+            side_effect=self._consumer_record_no_new_env,
+        ):
+            self.backend._check_output_exchange_sync()
         self.assertEqual(self.record.edi_exchange_state, "output_sent")
         self.assertEqual(
             responses.calls[0].request.headers["Content-Type"], "application/xml"
@@ -56,7 +68,13 @@ class TestSend(TestEDIWebserviceBase):
             body=response,
             status=401,
         )
-        self.backend._check_output_exchange_sync()
+
+        with mock.patch(
+            "odoo.addons.webservice.models.webservice_backend."
+            "WebserviceBackend._consumer_record_env",
+            side_effect=self._consumer_record_no_new_env,
+        ):
+            self.backend._check_output_exchange_sync()
         self.assertEqual(self.record.edi_exchange_state, "output_error_on_send")
         self.assertEqual(
             responses.calls[0].request.headers["Content-Type"], "application/xml"
@@ -72,13 +90,19 @@ class TestSend(TestEDIWebserviceBase):
         self.record._set_file_content(
             "first call content", field_name="ws_response_content"
         )
+
         with mock.patch(
-            "odoo.addons.webservice.components.request_adapter."
-            "BaseRestRequestsAdapter._get_url",
-            side_effect=Exception("Not an HTTPError"),
+            "odoo.addons.webservice.models.webservice_backend."
+            "WebserviceBackend._consumer_record_env",
+            side_effect=self._consumer_record_no_new_env,
         ):
-            with self.assertRaisesRegex(Exception, "Not an HTTPError"):
-                self.backend._check_output_exchange_sync()
+            with mock.patch(
+                "odoo.addons.webservice.components.request_adapter."
+                "BaseRestRequestsAdapter._get_url",
+                side_effect=Exception("Not an HTTPError"),
+            ):
+                with self.assertRaisesRegex(Exception, "Not an HTTPError"):
+                    self.backend._check_output_exchange_sync()
 
         self.assertFalse(self.record.ws_response_status_code)
         self.assertFalse(
